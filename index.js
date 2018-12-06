bfb63092698f58e9cbb4663cb0b335adc0e753a3
Dmitry Yakimov <welldan97@gmail.com>
2018-12-06 17:31:33 +0000

    United next.js and loopback

---
+++ index.js
@@ -0,0 +1,11 @@
+const client = require('./client');
+const serverApp = require('./server/dist/');
+
+client({
+  dir: 'client',
+  beforeNext: async server => {
+    const app = new serverApp.FrogfishServerApplication();
+    await app.boot();
+    server.use('/api', app.requestHandler);
+  },
+});
